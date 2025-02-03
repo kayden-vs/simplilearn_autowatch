d3d594a54b8becd8a19ef76f41a5690f0ab167a5
kayden-vs <rohitr9832@gmail.com>
2025-02-03 15:25:08 +0530

    video's playingg

--- video-bot.py
+++ video-bot.py
@@ -1,20 +1,3 @@
-# from selenium import webdriver
-# from selenium.webdriver.chrome.service import Service
-# from selenium.webdriver.common.by import By
-
-# # Set up ChromeDriver
-# service = Service("/usr/local/bin/chromedriver")
-# driver = webdriver.Chrome(service=service)
-
-# # Open a webpage
-# driver.get("https://www.google.com")
-
-# # Print page title
-# print("Page Title:", driver.title)
-
-# # Close browser
-# driver.quit()
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
@@ -25,7 +8,7 @@ from selenium.webdriver.support import expected_conditions as EC
 
 
 options = Options()
-# options.add_argument("--headless")  #use it later
+options.add_argument("--headless")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--autoplay-policy=no-user-gesture-required")
@@ -54,18 +37,19 @@ try:
 
     #Click Continue Learning
     driver.find_element(By.CSS_SELECTOR, "button.btn.prime.custom_button_ep.ng-star-inserted").click()
-    time.sleep(10)
+    time.sleep(15)
 
-    #Click the lesson name on right sidebar
-    driver.find_element(By.XPATH, "//p[@class='lesson-topic-tittle' and normalize-space(text())='5.1 Learning Objectives']").click()
+    #Click the self learning
+    driver.find_element(By.CSS_SELECTOR, "p.osl-section-name[title='Self learning']").click()
     time.sleep(5)
 
+    print("Watching the video..")
     # At this point, the video should be playing.
     # The script won't exit, so it will stay connected as long as it's running.
 
     while True:
         # This loop keeps the script running so the browser stays active
-        time.sleep(20)
+        time.sleep(30)
 
 except Exception as e:
     print(f"An error occurred: {e}")
